1ccc476078d14deadf8a59a946280b0fd438d145
JI3BA <lyovkin_artyom@mail.ru>
2021-04-19 12:27:07 +0300

    HW 35

--- lessons/35-npm/public-package/index.js
+++ lessons/35-npm/public-package/index.js
@@ -1,6 +1,9 @@
 // :)
 const smartSearch = (array, value) => array.includes(value);
 
+const randomSearch = (value) => array[Math.floor(Math.random() * value)];
+
 module.exports = {
   smartSearch,
+  randomSearch,
 }
