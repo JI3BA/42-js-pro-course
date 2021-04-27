6380e60bbb46bcdf97b768ca947370bb9a88a49e
JI3BA <lyovkin_artyom@mail.ru>
2021-04-28 00:38:18 +0300

    HW37

--- lessons/37_38-webpack/src/main.js
+++ lessons/37_38-webpack/src/main.js
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import Content from './components/Content'
 import Contacts from './components/Contacts'
 
+
 const render = () => {
   const root = document.querySelector('#app')
   root.innerHTML = ''
@@ -11,16 +12,16 @@ const render = () => {
     Header()
   )
   if (window.location.pathname === '/home' || window.location.pathname === '/') {
-    // import('./components/Content')
-    //   .then(({ default: contentModule }) => {
-    //     console.log(contentModule)
-    //     root.appendChild(contentModule())
-    //   })
     root.appendChild(
       Content()
     )
   }
   if (window.location.pathname === '/contacts') {
+    import('./components/Contacts')
+        .then(({ default: contactsModule }) => {
+          console.log(contactsModule)
+          root.appendChild(contactsModule())
+        })
     root.appendChild(
       Contacts()
     )
--- lessons/37_38-webpack/webpack.config.js
+++ lessons/37_38-webpack/webpack.config.js
@@ -1,1 +1,95 @@
-module.exports = {}
+const { resolve } = require('path')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+const productionPlugin = [
+    new MiniCssExtractPlugin(),
+]
+const config = {
+    devtool: 'source-map',
+    mode: isProduction ? 'production' : 'development',
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [isProduction ? MiniCssExtractPlugin.loader :'style-loader',
+                 'css-loader'
+                ],
+            },
+            {
+                test: /\.png|jpeg$/i,
+                use: ['file-loader'],
+            },
+            {
+                test: /\.woff2$/i,
+                use: ['file-loader'],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: [
+                      ['@babel/preset-env', { targets: "defaults" }]
+                    ]
+                  }
+                }
+            },
+        ]
+    },
+    resolve: {
+        modules: [
+            'node_modules',
+            'src',
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new ImageMinimizerPlugin({
+            minimizerOptions: {
+              plugins: [
+                ['jpegtran', { progressive: true }],
+                ['optipng', { optimizationLevel: 5 }],
+              ],
+            },
+          }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        })
+    ].concat(isProduction ? productionPlugin : []),
+    devServer: {
+        contentBase: '/.dist',
+        historyApiFallback : true,
+    },
+}
+
+if(isProduction){
+    config.optimization = {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+        ], 
+    }
+}
+
+if (module.hot) {
+    module.hot.accept('./components/Header', () => {
+      console.log('Accepting the updated printMe module!');
+      render();
+    })
+  }
+
+module.exports = config
